eb4adfb42c2ef5f789a3801a2c1b5a47c0a77147
Avi <Avi.Cohen@Qualit.co.il>
2020-11-05 10:03:38 +0200

    fix db connection

--- util/database.js
+++ util/database.js
@@ -5,12 +5,12 @@ const Sequelize = require('sequelize');
 //if in production mode database connection string is from enviroment varaibles
 let sequelize ;
 if (process.env.NODE_ENV === 'production'){
-     //sequelize = new Sequelize('test',process.env.RDS_USERNAME,process.env.RDS_PASSWORD,{dialect:'postgres', host:process.env.RDS_HOSTNAME });
-     sequelize = new Sequelize('test_db','postgres','Aa123456',{dialect:'postgres', host:'aatcgcgyxwa8a8.cywto99mqnpk.us-east-1.rds.amazonaws.com'});
+     sequelize = new Sequelize('test_db',process.env.RDS_USERNAME,process.env.RDS_PASSWORD,{dialect:'postgres', host:process.env.RDS_HOSTNAME });
+
 }
 //else connect via localhost
 else 
- sequelize = new Sequelize('test_db','postgres','Aa123456',{dialect:'postgres', host:'aatcgcgyxwa8a8.cywto99mqnpk.us-east-1.rds.amazonaws.com'});
+ sequelize = new Sequelize('test_db','postgres','Aa123456',{dialect:'postgres', host:'aa1kqchti5nf5nt.cywto99mqnpk.us-east-1.rds.amazonaws.com'});
      //sequelize = new Sequelize('intellident','postgres','Aa123456',{dialect:'postgres', host:'localhost'});
 
 module.exports = sequelize;
